7858a4577da01ae3a79759475a9ae7810ad9989a
Brian Ketelsen <bketelsen@gmail.com>
2021-05-10 07:27:24 -0400

    log dirname

--- api/index.js
+++ api/index.js
@@ -10,12 +10,13 @@ const app = require("express")();
 const middlewares = jsonServer.defaults({
   readOnly: true
 })
-
+console.log(__dirname);
 
 const router = jsonServer.router(data, { foreignKeySuffix: '_id' })
 router.use(middlewares)
 
 app.use("/api/graphql", jsonGraphqlExpress(data));
 app.use("/api/rest", router);
+
 
 module.exports = app
